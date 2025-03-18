0f092623e99ab8c9aba6d3ef75142d04356fd61a
Fiodar Siyanka <siyankafiodar@gmail.com>
2025-03-18 12:53:54 +0300

    chore: init husky

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error("Root element not found");
+}
+createRoot(container).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
